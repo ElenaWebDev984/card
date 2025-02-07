13bf4d938020a3ca6dec82b0b378cae2e34e755e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-07 17:47:30 +0100

    add Theme

---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,10 @@
+export const myTheme = {
+    colors: {
+        body: '#ede8e8',
+        button: {
+            primary: '#4E71FE',
+            secondary: 'transparent',
+            hover: '#1a1a91',
+        }
+    },
+}
